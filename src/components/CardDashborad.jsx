c4ae14763be12d285de5895b033aef3d0afbd550
NattawutTanthai <erictyth@gmail.com>
2023-04-19 12:36:33 +0700

    add comment

--- src/components/CardDashborad.jsx
+++ src/components/CardDashborad.jsx
@@ -47,7 +47,7 @@ export default function CardDashborad() {
             });
     }
 
-    useEffect(() => {
+    useEffect(() => { //getdata
         getWaitReport();
         getProcess();
         getSuccess();
