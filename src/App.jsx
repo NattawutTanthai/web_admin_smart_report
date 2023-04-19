5b3f9b9aa082fc5b0f0a7d92027c3cf0313eb16d
NattawutTanthai <erictyth@gmail.com>
2023-04-19 12:47:24 +0700

    fixed width

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import DrawerComp from './components/DrawerComp'
-import DashboradPage from './pages/HomePage'
-import EmployeePage from './pages/EmployeePage'
-import TypePage from './pages/TypePage'
 import LoginPage from './pages/LoginPage'
-import AdminPage from './pages/AdminPage'
 
 function App() {
   return (
