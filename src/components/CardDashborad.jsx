6fce559530695b46950531421336b65101f1282d
NattawutTanthai <erictyth@gmail.com>
2023-04-19 13:02:40 +0700

    fixed remove mui

--- src/components/CardDashborad.jsx
+++ src/components/CardDashborad.jsx
@@ -1,7 +1,7 @@
-import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
+// import Typography from '@mui/material/Typography';
+// import { styled } from '@mui/material/styles';
+// import Grid from '@mui/material/Grid';
+// import Box from '@mui/material/Box';
 import { useEffect, useState } from 'react';
 import Axios from '../../constants/axiosConfig';
 
@@ -54,51 +54,51 @@ export default function CardDashborad() {
 
     return (
         <>
-            <Box sx={{ width: '100%' }}>
-                {/* <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}> */}
-                {/* <Grid item xs={6}> */}
-                {/* <Item sx={{ backgroundColor: '#FFD457' }}> */}
+            {/* <Box sx={{ width: '100%' }}> */}
+            {/* <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}> */}
+            {/* <Grid item xs={6}> */}
+            {/* <Item sx={{ backgroundColor: '#FFD457' }}> */}
 
-                <Typography gutterBottom variant="h5" component="div">
-                    <b>รอรับเรื่อง</b>
-                </Typography>
-                <Typography variant="h6" color="text.secondary">
-                    ทั้งหมด {waitReport ?? "0"} รายการ
-                </Typography>
-                {/* </Item> */}
-                {/* </Grid> */}
-                {/* <Grid item xs={6}> */}
-                {/* <Item sx={{ backgroundColor: '#79AFFF' }}> */}
-                <Typography gutterBottom variant="h5" component="div">
-                    <b>ดำเนินการ</b>
-                </Typography>
-                <Typography variant="h6" color="text.danger">
-                    ทั้งหมด {process ?? "0"} รายการ
-                </Typography>
-                {/* </Item> */}
-                {/* </Grid> */}
-                {/* <Grid item xs={6}> */}
-                {/* <Item sx={{ backgroundColor: '#8AFF88' }}> */}
-                <Typography gutterBottom variant="h5" component="div">
-                    <b>เสร็จสิ้น</b>
-                </Typography>
-                <Typography variant="h6" color="text.secondary">
-                    ทั้งหมด {success ?? "0"} รายการ
-                </Typography>
-                {/* </Item> */}
-                {/* </Grid> */}
-                {/* <Grid item xs={6}> */}
-                {/* <Item sx={{ backgroundColor: '#FF8585' }}> */}
-                <Typography gutterBottom variant="h5" component="div">
-                    <b>ส่งต่อ</b>
-                </Typography>
-                <Typography variant="h6" color="text.secondary">
-                    ทั้งหมด 65 รายการ
-                </Typography>
-                {/* </Item> */}
-                {/* </Grid> */}
-                {/* </Grid> */}
-            </Box>
+            {/* <Typography gutterBottom variant="h5" component="div"> */}
+            <b>รอรับเรื่อง</b>
+            {/* </Typography> */}
+            {/* <Typography variant="h6" color="text.secondary"> */}
+            ทั้งหมด {waitReport ?? "0"} รายการ
+            {/* </Typography> */}
+            {/* </Item> */}
+            {/* </Grid> */}
+            {/* <Grid item xs={6}> */}
+            {/* <Item sx={{ backgroundColor: '#79AFFF' }}> */}
+            {/* <Typography gutterBottom variant="h5" component="div"> */}
+            <b>ดำเนินการ</b>
+            {/* </Typography> */}
+            {/* <Typography variant="h6" color="text.danger"> */}
+            ทั้งหมด {process ?? "0"} รายการ
+            {/* </Typography> */}
+            {/* </Item> */}
+            {/* </Grid> */}
+            {/* <Grid item xs={6}> */}
+            {/* <Item sx={{ backgroundColor: '#8AFF88' }}> */}
+            {/* <Typography gutterBottom variant="h5" component="div"> */}
+            <b>เสร็จสิ้น</b>
+            {/* </Typography> */}
+            {/* <Typography variant="h6" color="text.secondary"> */}
+            ทั้งหมด {success ?? "0"} รายการ
+            {/* </Typography> */}
+            {/* </Item> */}
+            {/* </Grid> */}
+            {/* <Grid item xs={6}> */}
+            {/* <Item sx={{ backgroundColor: '#FF8585' }}> */}
+            {/* <Typography gutterBottom variant="h5" component="div"> */}
+            <b>ส่งต่อ</b>
+            {/* </Typography> */}
+            {/* <Typography variant="h6" color="text.secondary"> */}
+            ทั้งหมด 65 รายการ
+            {/* </Typography> */}
+            {/* </Item> */}
+            {/* </Grid> */}
+            {/* </Grid> */}
+            {/* </Box> */}
         </>
     );
 }
