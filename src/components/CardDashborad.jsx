468b5d246d7da997be2f5d9eb462274ea0ffc865
NattawutTanthai <erictyth@gmail.com>
2023-04-19 12:55:23 +0700

    fixed width

--- src/components/CardDashborad.jsx
+++ src/components/CardDashborad.jsx
@@ -1,18 +1,17 @@
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { useEffect, useState } from 'react';
 import Axios from '../../constants/axiosConfig';
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//     ...theme.typography.body2,
+//     padding: theme.spacing(1),
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+// }));
 
 export default function CardDashborad() {
     const [waitReport, setWaitReport] = useState(null);
@@ -58,7 +57,7 @@ export default function CardDashborad() {
             <Box sx={{ width: '100%' }}>
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                     <Grid item xs={6}>
-                        <Item sx={{ backgroundColor: '#FFD457' }}>
+                        {/* <Item sx={{ backgroundColor: '#FFD457' }}> */}
 
                             <Typography gutterBottom variant="h5" component="div">
                                 <b>รอรับเรื่อง</b>
@@ -66,37 +65,37 @@ export default function CardDashborad() {
                             <Typography variant="h6" color="text.secondary">
                                 ทั้งหมด {waitReport ?? "0"} รายการ
                             </Typography>
-                        </Item>
+                        {/* </Item> */}
                     </Grid>
                     <Grid item xs={6}>
-                        <Item sx={{ backgroundColor: '#79AFFF' }}>
+                        {/* <Item sx={{ backgroundColor: '#79AFFF' }}> */}
                             <Typography gutterBottom variant="h5" component="div">
                                 <b>ดำเนินการ</b>
                             </Typography>
                             <Typography variant="h6" color="text.danger">
                                 ทั้งหมด {process ?? "0"} รายการ
                             </Typography>
-                        </Item>
+                        {/* </Item> */}
                     </Grid>
                     <Grid item xs={6}>
-                        <Item sx={{ backgroundColor: '#8AFF88' }}>
+                        {/* <Item sx={{ backgroundColor: '#8AFF88' }}> */}
                             <Typography gutterBottom variant="h5" component="div">
                                 <b>เสร็จสิ้น</b>
                             </Typography>
                             <Typography variant="h6" color="text.secondary">
                                 ทั้งหมด {success ?? "0"} รายการ
                             </Typography>
-                        </Item>
+                        {/* </Item> */}
                     </Grid>
                     <Grid item xs={6}>
-                        <Item sx={{ backgroundColor: '#FF8585' }}>
+                        {/* <Item sx={{ backgroundColor: '#FF8585' }}> */}
                             <Typography gutterBottom variant="h5" component="div">
                                 <b>ส่งต่อ</b>
                             </Typography>
                             <Typography variant="h6" color="text.secondary">
                                 ทั้งหมด 65 รายการ
                             </Typography>
-                        </Item>
+                        {/* </Item> */}
                     </Grid>
                 </Grid>
             </Box>
